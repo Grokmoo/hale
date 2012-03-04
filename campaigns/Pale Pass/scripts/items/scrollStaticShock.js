4b577eda0a6002abca373dd5d72d26c15b2b3b1d
grokmoo <Grokmoo@users.noreply.github.com>
2012-03-04 23:40:23 +0000

    fixed error in shock scroll, #120

--- campaigns/Pale Pass/scripts/items/scrollStaticShock.js
+++ campaigns/Pale Pass/scripts/items/scrollStaticShock.js
@@ -1,10 +1,10 @@
 function canUse(game, item, target) {
-    return target.getTimer().canActivateAbility("StaticShock");
+    return target.getTimer().canActivateAbility("Shock");
 }
 
 function onUse(game, item, target) {
     var quality = item.getQuality().getModifier() + 100;
-    var caster = game.createScrollCaster("StaticShock")
+    var caster = game.createScrollCaster("Shock")
     
     caster.setItemToUse(item);
     caster.setParent(target);
