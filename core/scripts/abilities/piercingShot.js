05dba89b539aa9acc23dd9a1d730ebcc93de43d5
grokmoo <Grokmoo@users.noreply.github.com>
2012-04-20 02:20:29 +0000

    fixed piercing shot seeming to not do anything

--- core/scripts/abilities/piercingShot.js
+++ core/scripts/abilities/piercingShot.js
@@ -22,10 +22,29 @@ function onTargetSelect(game, targeter) {
 	
 	parent.getTimer().performAttack();
 	
+	var g1 = game.getBaseParticleGenerator("ray");
+	g1.setRedDistribution(game.getFixedDistribution(0.4));
+	g1.setGreenDistribution(game.getFixedDistribution(0.28));
+	g1.setBlueDistribution(game.getFixedDistribution(0.22));
+	g1.setAlphaSpeedDistribution(game.getFixedDistribution(-1.0));
+	g1.setVelocityDurationRotationBasedOnSpeed(parent.getPosition(), targeter.getEndPoint(), 600.0);
+	
 	var targets = targeter.getAffectedCreatures();
 	for (var i = 0; i < targets.size(); i++) {
 		var target = targets.get(i);
+	
+		var delay = targets.get(i).getPosition().screenDistance(parent.getPosition()) / g1.getSpeed();
 		
-		game.singleAttack(parent, target);
+		var callback = ability.createDelayedCallback("applyDamage");
+		callback.setDelay(delay);
+		callback.addArguments([parent, target]);
+		callback.start();
 	}
+	
+	game.runParticleGeneratorNoWait(g1);
+	game.lockInterface(g1.getTimeLeft());
+}
+
+function applyDamage(game, parent, target) {
+	game.singleAttack(parent, target);
 }
