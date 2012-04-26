c0ff1968d1393227446c216c98a1e3d618ac67da
grokmoo <grokmoo@810a1811-442c-4c41-aec8-81d22a9ba754>
2012-04-26 14:07:53 +0000

    set up framework for pale pass chapter 3

---
+++ trunk/campaigns/Pale Pass 3/scripts/items/scrollShield.js
@@ -0,0 +1,13 @@
+function canUse(game, item, target) {
+	return target.getTimer().canActivateAbility("Shield");
+}
+
+function onUse(game, item, target) {
+	var quality = item.getQuality().getModifier() + 100;
+	
+	var caster = game.createScrollCaster("Shield");
+	caster.setItemToUse(item);
+	caster.setParent(target);
+	caster.setCasterLevel(quality / 25);
+	caster.activate();
+}
