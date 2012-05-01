97dd1a6051349c7569d2584f4fadd9843f76869a
grokmoo <Grokmoo@users.noreply.github.com>
2012-05-01 15:49:40 +0000

    finished up tomb of narkel area (ticket #130)

--- campaigns/Pale Pass 3/scripts/quests/theMaster.js
+++ campaigns/Pale Pass 3/scripts/quests/theMaster.js
@@ -11,17 +11,33 @@ function startQuest(game) {
 }
 
 function learnOfArmy(game) {
-	var quest = game.getQuestEntry("The Master");
-	
-	if (quest.hasSubEntry("The Master's Army")) return;
-	
-	quest.setCurrentSubEntriesCompleted();
-	
-	var entry = quest.createSubEntry("The Master's Army");
-	
-	entry.addText("You met a scout who told you that while you were trapped underground, the Master has assembled an army and is now openly opposing the King.");
-	
-	entry.addText("You should travel to Aravil to learn more and help with the fight against the Master.");
-	
-	
+    var quest = game.getQuestEntry("The Master");
+    
+    if (quest.hasSubEntry("The Master's Army")) return;
+    
+    quest.setCurrentSubEntriesCompleted();
+    
+    var entry = quest.createSubEntry("The Master's Army");
+    
+    entry.addText("You met a scout who told you that while you were trapped underground, the Master has assembled an army and is now openly opposing the King.");
+    
+    entry.addText("You should travel to Aravil to learn more and help with the fight against the Master.");
+}
+
+function narkel(game) {
+    var quest = game.getQuestEntry("The Master");
+    
+    if (quest.hasSubEntry("The Binding Focus")) return;
+    
+    quest.setCurrentSubEntriesCompleted();
+    
+    var entry = quest.createSubEntry("The Binding Focus");
+    
+    entry.addText("Deep in his ancient tomb, you found and spoke with the spirit of the warrior, Narkel.");
+    
+    entry.addText("He informed you that the Master is an ancient demon, bound to this world through a powerful spell requiring some manner of focus.");
+    
+    entry.addText("If the focus is destroyed, the Master should be removed from this plane.  Unfortunately, Narkell did not know where the focus would be.");
+    
+    entry.addText("His only clues were that the focus must be a large and pure crystal, and that the Master would most likely keep it close to him.");
 }
