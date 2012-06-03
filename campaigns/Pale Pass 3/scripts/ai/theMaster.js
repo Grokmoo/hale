817aec77a24798c745278df052570a409d0f3f21
grokmoo <Grokmoo@users.noreply.github.com>
2012-06-03 21:00:23 +0000

    fixed issue #181 (game ends too quickly after defeating the master)

--- campaigns/Pale Pass 3/scripts/ai/theMaster.js
+++ campaigns/Pale Pass 3/scripts/ai/theMaster.js
@@ -1,16 +1,16 @@
 function cutsceneFinished(game) {
     var popup = game.showCampaignConclusionPopup();
     popup.addText("<div style=\"font-family: vera;\">");
-	popup.addText("Congratulations on completing the Pale Pass campaign.");
-	popup.addText("</div>");
-	
-	popup.addText("<div style=\"font-family: vera; margin-top: 1em;\">");
-	popup.addText("Thanks for playing!");
-	popup.addText("</div>");
-	
-	popup.setTextAreaHeight(120);
-	
-	popup.show();
+    popup.addText("Congratulations on completing the Pale Pass campaign.");
+    popup.addText("</div>");
+    
+    popup.addText("<div style=\"font-family: vera; margin-top: 1em;\">");
+    popup.addText("Thanks for playing!");
+    popup.addText("</div>");
+    
+    popup.setTextAreaHeight(120);
+    
+    popup.show();
 }
 
 function startCutscene(game) {
@@ -18,10 +18,32 @@ function startCutscene(game) {
 }
 
 function onCreatureDeath(game, parent) {
-	game.lockInterface(3.0);
-    game.runExternalScriptWait("ai/theMaster", "startCutscene", 3.0);
+    game.lockInterface(3.0);
+	
+	game.runExternalScriptWait("ai/theMaster", "spawnPortal", 1.0);
+    
+    game.revealArea(37, 7, 2, 0);
+}
+
+function spawnPortal(game) {
+    var portalCreature = game.entities().getCreature("theMasterPortal");
+	portalCreature.setPosition(37, 7);
+	portalCreature.setFaction("Neutral");
+	
+	game.currentArea().getEntities().addEntity(portalCreature);
+	
+	var effect = portalCreature.createEffect();
+	effect.setHasDescription(false);
+	
+	var g1 = game.getBaseParticleGenerator("sparkle");
+	g1.setDurationInfinite();
+	g1.setRotationSpeedDistribution(game.getUniformDistribution(100.0, 200.0));
+	g1.setPosition(portalCreature.getPosition());
+	effect.addAnimation(g1);
+	
+	portalCreature.applyEffect(effect);
 }
 
 function runTurn(game, parent) {
-	game.runExternalScript("ai/aiStandard", "runTurn", parent);
+    game.runExternalScript("ai/aiStandard", "runTurn", parent);
 }
