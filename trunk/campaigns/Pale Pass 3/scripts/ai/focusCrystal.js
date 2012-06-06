592e88a21d8caec3a1f0dab217aa8e2188f60f8f
grokmoo <grokmoo@810a1811-442c-4c41-aec8-81d22a9ba754>
2012-06-06 21:50:35 +0000

    fixed script error caused by killing the master before destroying the focus crystal

--- trunk/campaigns/Pale Pass 3/scripts/ai/focusCrystal.js
+++ trunk/campaigns/Pale Pass 3/scripts/ai/focusCrystal.js
@@ -7,12 +7,19 @@ function startConvo(game, player) {
 }
 
 function onCreatureDeath(game, parent) {
+	// check to make sure the player hasn't already somehow killed the master
+	var target = game.currentArea().getEntityWithID("theMaster");
+	if (target == null) return;
+
 	game.lockInterface(3.0);
     game.runExternalScriptWait("ai/focusCrystal", "startConvo", 1.0, game.getParty().get(0));
 }
 
 function runTurn(game, parent) {
 	var target = game.currentArea().getEntityWithID("theMaster");
+	
+	// check to make sure the player hasn't already somehow killed the master
+	if (target == null) return;
 	
 	var damage = target.stats().getMaxHP() - target.getCurrentHP();
 	
