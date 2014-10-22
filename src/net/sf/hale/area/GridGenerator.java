a1dbb73b3cf09afd94da8b47f453ff3d2dc8eb3c
grokmoo <Grokmoo@users.noreply.github.com>
2014-10-22 16:40:48 +0000

    set up grid based maze generator

---
+++ src/net/sf/hale/area/GridGenerator.java
@@ -0,0 +1,133 @@
+package net.sf.hale.area;
+
+import net.sf.hale.Game;
+import net.sf.hale.rules.Dice;
+import net.sf.hale.tileset.Tileset;
+import net.sf.hale.util.SimpleJSONObject;
+
+/**
+ * breaks the area down into a random grid and builds a maze
+ * using the walls of the grid
+ * @author Jared
+ *
+ */
+
+public class GridGenerator implements Generator {
+	private Dice random;
+	private final Area area;
+	private final Tileset tileset;
+	
+	private final int gridSize;
+	private final int gridWidth, gridHeight;
+	
+	/**
+	 * Creates a new generator for the specified area
+	 * @param area
+	 * @param random
+	 */
+	
+	public GridGenerator(Area area, SimpleJSONObject data) {
+		this.area = area;
+		this.tileset = Game.curCampaign.getTileset(area.getTileset());
+		
+		this.gridSize = data.get("gridSize", 0);
+		
+		gridWidth = area.getWidth() / gridSize;
+		gridHeight = area.getHeight() / gridSize;
+	}
+	
+	@Override public void setDice(Dice random) {
+		this.random = random;
+	}
+	
+	/**
+	 * Generates tiles with this generator
+	 */
+	
+	@Override public void generate() {
+		int[][] cells = new int[gridWidth][gridHeight];
+		generateMaze(cells, 0, 0);
+		
+//		// generate a simple maze
+//		int[][] elev = elevationMazeDepthFirst();
+//
+//		// set elevation grid
+//		for (int x = 0; x < area.getWidth(); x++) {
+//			for (int y = 0; y < area.getHeight(); y++) {
+//				area.getElevationGrid().setElevation(x, y, (byte)elev[x][y]);
+//			}
+//		}
+//
+//		// add elevation border tiles
+//		for (int x = 0; x < area.getWidth(); x++) {
+//			for (int y = 0; y < area.getHeight(); y++) {
+//				Point p = new Point(x, y);
+//
+//				for (ElevationList.Elevation elevation : tileset.getElevationList().
+//						getMatchingElevationRules(area.getElevationGrid(), p)) {
+//
+//					Border border = elevation.getBorder();
+//
+//					for (BorderTile borderTile : border) {
+//						Point borderPoint = borderTile.getPosition().getRelativePoint(p);
+//
+//						PointImmutable bP = new PointImmutable(borderPoint);
+//						if (!bP.isWithinBounds(area)) continue;
+//
+//						area.getTileGrid().addTile(borderTile.getID(), borderTile.getLayerID(), bP.x, bP.y);
+//					}
+//				}
+//			}
+//		}
+	}
+	
+	private void generateMaze(int[][] cells, int x, int y) {
+		Direction[] directions = Direction.values();
+
+		// shuffle array
+		for (int i = directions.length - 1; i > 0; i--) {
+			int index = random.rand(0, i);
+			
+			Direction d = directions[index];
+			directions[index] = directions[i];
+			directions[i] = d;
+		}
+		
+		for (Direction direction : directions) {
+			int newX = x + direction.dx;
+			int newY = y + direction.dy;
+			
+			if (newX < 0 || newX >= gridWidth || newY < 0 || newY >= gridHeight) {
+				continue;
+			}
+			
+			if (cells[newX][newY] == 0) {
+				cells[x][y] |= direction.bit;
+				cells[newX][newY] |= direction.opposite.bit;
+				generateMaze(cells, newX, newY);
+			}
+		}
+	}
+	
+	private enum Direction {
+		North(1, 0, -1), South(2, 0, 1), East(4, 1, 0), West(8, -1, 0);
+		
+		private final int bit;
+		private final int dx;
+		private final int dy;
+		private Direction opposite;
+ 
+		static {
+			North.opposite = South;
+			South.opposite = North;
+			East.opposite = West;
+			West.opposite = East;
+		}
+ 
+		private Direction(int bit, int dx, int dy) {
+			this.bit = bit;
+			this.dx = dx;
+			this.dy = dy;
+		}
+	};
+}
