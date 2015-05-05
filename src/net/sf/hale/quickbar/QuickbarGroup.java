ffe4b6f24cb1cb3e2370d4dcc9577959e9e3a8fb
grokmoo <Grokmoo@users.noreply.github.com>
2015-05-05 16:37:19 +0000

    began work on quickbar groups - better (and automatic) organization of the quickbar

---
+++ src/net/sf/hale/quickbar/QuickbarGroup.java
@@ -0,0 +1,73 @@
+/*
+ * Hale is highly moddable tactical RPG.
+ * Copyright (C) 2015 Jared Stephen
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package net.sf.hale.quickbar;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import net.sf.hale.ability.Ability;
+import net.sf.hale.util.SimpleJSONObject;
+
+/**
+ * a set of activateable abilities listed together under a common grouping in the quickbar
+ * @author Jared
+ *
+ */
+
+public class QuickbarGroup {
+	private final String name;
+	private final List<Ability> abilities;
+	
+	/**
+	 * Creates a new QuickbarGroup by parsing the JSON
+	 * @param data
+	 * @param abilitiesByGroup
+	 */
+	
+	public QuickbarGroup(SimpleJSONObject data, Map<String, List<Ability>> abilitiesByGroup) {
+		this.name = data.get("name", null);
+		
+		List<Ability> abilities = abilitiesByGroup.get(this.name);
+		if (abilities == null) {
+			this.abilities = Collections.emptyList();
+		} else {
+			this.abilities = Collections.unmodifiableList(abilities);
+		}
+		
+//		System.out.println("Created group " + name + " with " + abilities.size() + " abilities:");
+//		System.out.print("   ");
+//		for (Ability ability : abilities) {
+//			System.out.print(ability.getID());
+//			System.out.print(" ");
+//		}
+//		System.out.println();
+	}
+	
+	public String getName() { return name; }
+	
+	/**
+	 * Returns the list of abilities in this group.  Note that this list may not be
+	 * modified
+	 * @return the list of abilities in this group
+	 */
+	
+	public List<Ability> getAbilities() { return abilities; }
+}
