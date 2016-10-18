eee9808e362684f0657394e19d008b86f9883f8e
Jared Stephen <grok_moo@yahoo.com>
2016-10-18 12:14:52 -0400

    added monk ability chi mastery

--- core/scripts/abilities/bodyMastery.js
+++ core/scripts/abilities/bodyMastery.js
@@ -1,21 +1,14 @@
 function onActivate(game, slot) {
-	var targeter = game.createListTargeter(slot);
-	targeter.addAllowedCreature(slot.getParent());
-	targeter.activate();
-}
-
-function onTargetSelect(game, targeter) {
-	var ability = targeter.getSlot().getAbility();
-	var parent = targeter.getParent();
-	var target = targeter.getSelectedCreature();
+	var ability = slot.getAbility();
+	var parent = slot.getParent();
 
 	var duration = parseInt(2 + parent.roles.getLevel("Monk") / 4);
 	
 	// cast the ability
-	targeter.getSlot().setActiveRoundsLeft(duration);
-	targeter.getSlot().activate();
+	slot.setActiveRoundsLeft(duration);
+	slot.activate();
    
-	var effect = targeter.getSlot().createEffect();
+	var effect = slot.createEffect();
 	effect.setDuration(duration);
 	effect.addPositiveIcon("items/enchant_physical_small");
 	effect.setTitle(ability.getName());
@@ -24,14 +17,18 @@ function onTargetSelect(game, targeter) {
 	effect.getBonuses().addBonus('ReflexResistance', 'Stackable', 25);
 	effect.getBonuses().add("ImmobilizationImmunity");
 	
+	if (parent.abilities.has("ChiMastery")) {
+		effect.getBonuses().addBonus('SpellResistance', parent.roles.getLevel("Monk") * 4);
+	}
+	
 	var g1 = game.getBaseParticleGenerator("sparkle");
 	g1.setDurationInfinite();
 	g1.setRotationSpeedDistribution(game.getUniformDistribution(100.0, 200.0));
-	g1.setPosition(target.getLocation());
+	g1.setPosition(parent.getLocation());
 	g1.setRedDistribution(game.getUniformDistribution(0.4, 0.6));
 	g1.setGreenDistribution(game.getUniformDistribution(0.2, 0.3));
 	g1.setBlueDistribution(game.getUniformDistribution(0.5, 0.8));
 	effect.addAnimation(g1);
 	
-	target.applyEffect(effect);
+	parent.applyEffect(effect);
 }
